7fc882b1ead434e21dd539479966ba5e4533a78b
Guilherme Knust <gknust.b@gmail.com>
2026-03-31 12:34:07 -0300

    refactor: refactored main into multiple functions for better exception handling and modularity, modified .gitignore

--- src/main/java/com/gknust/App.java
+++ src/main/java/com/gknust/App.java
@@ -1,18 +1,35 @@
 package com.gknust;
 
+import java.io.IOException;
 import java.sql.*;
 
 public class App {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     try {
-      Connection connection = DriverManager.getConnection(System.getenv("DB_URL"), System.getenv("DB_USER"),
-          System.getenv("DB_PASSWORD"));
-      Statement stmt = connection.createStatement();
-      stmt.executeUpdate("CREATE TABLE a (id int not null primary key, value varchar(20))");
-      stmt.close();
-      connection.close();
-    } catch (Exception e) {
-      System.out.println("hi!");
+      sqlTest();
+    } catch (SQLException e) {
+      for (Throwable t : e)
+        t.printStackTrace();
     }
+  }
+
+  public static void sqlTest() throws SQLException, IOException {
+    try (Connection connection = getConnection(); Statement stat = connection.createStatement()) {
+      stat.executeUpdate("CREATE TABLE foo(message VARCHAR(30))");
+      stat.executeUpdate("INSERT INTO foo VALUES ('Hello, World!')");
+
+      try (ResultSet result = stat.executeQuery("SELECT * FROM foo")) {
+        if (result.next())
+          System.out.println(result.getString(1));
+      }
+      stat.executeUpdate("DROP TABLE foo");
+    }
+  }
+
+  public static Connection getConnection() throws SQLException, IOException {
+    String dbUrl = System.getenv("DB_URL");
+    String dbUser = System.getenv("DB_USER");
+    String dbPassword = System.getenv("DB_PASSWORD");
+    return DriverManager.getConnection(dbUrl, dbUser, dbPassword);
   }
 }
